746e24e59a0a2943185d88f9f48e5b5798584ef3
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2016-12-29 15:41:14 -0500

    Succesfull retrieval of JSON

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,35 @@
+//// Global variables
+
+var queryParameters = '&units=metric&APPID=6f36f42e1677237f380a89ee3b72edec'
+var API = 'http://api.openweathermap.org/data/2.5/weather'
+var defaultT = 25;
+var defaultS = 5;
+var defaultD = 45;
+var defaultCities = ["Kathmandu", "Lhasa", "İstanbul", "Pya", "Hokkaidō", 
+                    "Lisbon", "Helsinki", "Osuna", "Northwich", "Villa del Rosario", 
+                    "Santa Marta", "Leticia", "Cali"]
+
+//// Event Listeners
+
 function watchForSubmit() {
     $('.js-city-form').on('click', '.js-city-submit', function(event) {
         event.preventDefault()
-        transitionHelp()
+        if (!($('.js-city-name').val())) {
+             $( ".js-city-name" ).fadeOut('fast')
+             $( ".js-city-name" ).fadeIn('fast')
+            return;
+        }
+        var inputCity = $('.js-city-name').val();   
+        handleWeather(inputCity)
+    })
+}
+
+function watchForRandom() {
+    $('.js-city-form').on('click', '.js-city-random', function(event){
+        event.preventDefault()
+        var randomCity = defaultCities[Math.floor(Math.random()*defaultCities.length)]
+        $('.js-city-name').val(randomCity)
+        handleWeather(randomCity)
     })
 }
 
@@ -11,6 +39,33 @@ function watchForHelp() {
     })
 }
 
+//// Helper methods
+
+
+function plotWeather(obj) {
+    console.dir(obj)
+}
+
+function handleWeather(city) {
+    var weather = fetchWeather(city)
+    plotWeather(weather)
+}
+
+// return object containing weather factors required to visualize:
+// temperature (T) in celsius and wind speed (S) in meters/second + direction (D) in degrees
+function fetchWeather(city) {
+    var cleanWeather = {};
+    $.getJSON(API, 'q='+city+queryParameters, function(obj) {
+        // Not all fetched objects have all the required keys
+        cleanWeather.T = (obj.main.temp) ? obj.main.temp : defaultT;
+        cleanWeather.S = (obj.wind.speed) ? obj.wind.speed : defaultS;
+        cleanWeather.D = (obj.wind.deg) ? obj.wind.deg : defaultD;
+    })
+
+    return cleanWeather;
+}
+
+// Toggle help menu
 function transitionHelp() {
     if($('.js-help-text').css('display') == 'none'){
             $('header').fadeOut('slow')
@@ -24,7 +79,7 @@ function transitionHelp() {
 }
 
 $(function(){
-    $('.js-help-text').hide();
     watchForSubmit()
+    watchForRandom()
     watchForHelp()
 })
