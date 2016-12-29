2bf9462ccd01127d70da52323c542232f459e7a4
CamiloGarciaLaRotta <camilo.garcia.larotta@gmail.com>
2016-12-29 14:03:44 -0500

    First tests with jQuery

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,30 @@
-visualize() {
-    var inputCity = $('#city-name')
-    alert(inputCity.val())
+function watchForSubmit() {
+    $('.js-city-form').on('click', '.js-city-submit', function(event) {
+        event.preventDefault()
+        transitionHelp()
+    })
+}
+
+function watchForHelp() {
+    $('.js-help-btn').click(function(e) {
+        transitionHelp()
+    })
+}
+
+function transitionHelp() {
+    if($('.js-help-text').css('display') == 'none'){
+            $('header').fadeOut('slow')
+            $('.js-help-text').fadeIn('slow')
+            $('.js-help-btn').attr('src', 'images/close.png')
+        } else {
+            $('.js-help-text').fadeOut("slow")
+            $('header').fadeIn("slow")
+            $('.js-help-btn').attr('src', 'images/help.png')
+    }
 }
 
 $(function(){
-    watch
+    $('.js-help-text').hide();
+    watchForSubmit()
+    watchForHelp()
 })
