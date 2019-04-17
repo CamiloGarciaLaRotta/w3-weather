58b9c960beb5167adb05d496fd59b1890956d30a
cegal <camilo.garcia.larotta@gmail.com>
2019-04-17 17:17:08 -0400

    refactor: updated js syntax

--- scripts/app.js
+++ scripts/app.js
@@ -1,106 +1,117 @@
-//// Global variables
+const CORS_WRAPPER = 'https://cors-anywhere.herokuapp.com/';
+const API = 'http://api.openweathermap.org/data/2.5/weather';
+const queryParameters = '&units=metric&APPID=6f36f42e1677237f380a89ee3b72edec';
 
-var queryParameters = '&units=metric&APPID=6f36f42e1677237f380a89ee3b72edec'
-var API = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather'
-var defaultT = 25;
-var defaultS = 5;
-var defaultD = 45;
+let defaultT = 25;
+let defaultS = 5;
+let defaultD = 45;
 // to account for the coordinate system of p5.js
-var Dcorrection = 0;
-var defaultCities = ["Kathmandu", "Lhasa", "İstanbul", "Pya", "Hokkaidō",
-    "Lisbon", "Helsinki", "Osuna", "Northwich", "Villa del Rosario",
-    "Santa Marta", "Leticia", "Cali", "Gibraltar"]
+let Dcorrection = 0;
+
+let defaultCities = [
+    "Kathmandu", "Lhasa", "İstanbul", "Pya", "Hokkaidō", "Lisbon", "Helsinki", "Osuna",
+    "Northwich", "Villa del Rosario", "Santa Marta", "Leticia", "Cali", "Gibraltar", "Kabul",
+    "Tirana", "Algiers", "Andorra la Vella", "Luanda", "Saint John", "Buenos Aires", "Yerevan",
+    "Canberra", "Vienna", "Baku", "Nassau", "Manama", "Dhaka", "Bridgetown", "Minsk", "Brussels",
+    "Belmopan", "Porto-Novo", "Thimphu", "La Paz", "Sucre", "Sarajevo", "Gaborone", "Brasilia",
+    "Bandar Seri Begawan", "Sofia", "Ouagadougou", "Phnom Penh", "Yaounde", "Ottawa", "Praia",
+    "Bangui", "N'Djamena", "Santiago", "Beijing", "Bogota", "Moroni", "Brazzaville", "Kinshasa",
+    "San Jose", "Abidjan", "Zagreb", "Havana", "Nicosia", "Prague", "Copenhagen", "Djibouti",
+    "Roseau", "Santo Domingo", "Dili", "Quito", "Cairo", "San Salvador", "Malabo", "Asmara",
+    "Tallinn", "Addis Ababa", "Suva", "Helsinki", "Paris", "Libreville", "Banjul", "Tbilisi",
+    "Berlin", "Accra", "Athens", "Saint George's", "Guatemala City", "Conakry", "Bissau",
+    "Georgetown", "Port-au-Prince", "Tegucigalpa", "Budapest", "Reykjavik", "New Delhi", "Jakarta",
+    "Tehran", "Baghdad", "Dublin", "Jerusalem*", "Rome", "Kingston", "Tokyo", "Amman", "Astana",
+    "Nairobi", "Tarawa Atoll", "Pyongyang", "Seoul", "Pristina", "Kuwait City", "Bishkek",
+    "Vientiane", "Riga", "Beirut", "Maseru", "Monrovia", "Tripoli", "Vaduz", "Vilnius",
+    "Luxembourg", "Skopje", "Antananarivo", "Lilongwe", "Kuala Lumpur", "Male", "Bamako",
+    "Valletta", "Majuro", "Nouakchott", "Port Louis", "Mexico City", "Chisinau",
+    "Monaco", "Ulaanbaatar", "Podgorica", "Rabat", "Maputo", "Rangoon", "Windhoek",
+    "Amsterdam", "Wellington", "Managua", "Niamey", "Abuja", "Oslo", "Muscat", "Islamabad",
+    "Melekeok", "Panama City", "Port Moresby", "Asuncion", "Lima", "Manila", "Warsaw", "Doha",
+    "Bucharest", "Moscow", "Kigali", "Basseterre", "Castries", "Kingstown", "Apia", "San Marino",
+    "Sao Tome", "Riyadh", "Dakar", "Belgrade", "Victoria", "Freetown", "Singapore", "Bratislava",
+    "Ljubljana", "Honiara", "Mogadishu", "Pretoria", "Cape Town", "Juba ", "Madrid", "Colombo",
+    "Khartoum", "Paramaribo", "Mbabane", "Stockholm", "Bern", "Damascus", "Taipei", "Dushanbe",
+    "Dodoma", "Bangkok", "Lome", "Nuku'alofa", "Port-of-Spain", "Tunis", "Ankara", "Ashgabat",
+    "Kampala", "Kyiv", "Abu Dhabi", "London", "Washington", "Montevideo", "Tashkent", "Port-Vila",
+    "Vatican City", "Caracas", "Hanoi", "Sanaa", "Lusaka", "Harare"];
 
 //// Event Listeners
-
-function watchForSubmit() {
-    $('.js-city-form').on('click', '.js-city-submit', function (event) {
-        event.preventDefault()
+let watchForSubmit = () => {
+    $('.js-city-form').on('click', '.js-city-submit', ev => {
+        ev.preventDefault();
         if (!($('.js-city-name').val())) {
-            $(".js-city-name").fadeOut('fast')
-            $(".js-city-name").fadeIn('fast')
+            $(".js-city-name").fadeOut('fast');
+            $(".js-city-name").fadeIn('fast');
             return;
         }
-        var inputCity = $('.js-city-name').val();
-        handleWeather(inputCity)
+        let inputCity = $('.js-city-name').val();
+        handleWeather(inputCity);
     })
-}
+};
 
-function watchForRandom() {
-    $('.js-city-form').on('click', '.js-city-random', function (event) {
-        event.preventDefault()
-        var randomCity = defaultCities[Math.floor(Math.random() * defaultCities.length)]
-        $('.js-city-name').val(randomCity)
-        handleWeather(randomCity)
+let watchForRandom = () => {
+    $('.js-city-form').on('click', '.js-city-random', ev => {
+        ev.preventDefault();
+        let randomCity = defaultCities[Math.floor(Math.random() * defaultCities.length)];
+        $('.js-city-name').val(randomCity);
+        handleWeather(randomCity);
     })
-}
+};
 
-function watchForHelp() {
-    $('.js-help-btn').click(function (e) {
-        transitionHelp()
-    })
-}
+let watchForHelp = () => $('.js-help-btn').click(_ => transitionHelp());
 
 //// Helper methods
+let plotWeather = obj => {
+    let directioninRads = toRads(obj.D);
+    setParticleParams(obj.S * Math.sin(directioninRads), obj.S * Math.cos(directioninRads), obj.D, obj.T);
+    $('.js-output-weather').html(
+        '<p> Temperature:' + obj.T + '&#176;  Wind Speed: ' +
+        obj.S + 'm/s  Direction: ' + obj.D + '&#176;</p>');
 
-function plotWeather(obj) {
-    console.dir(obj)
-    var directioninRads = toRads(obj.D);
-    setParticleParams(obj.S * Math.sin(directioninRads), obj.S * Math.cos(directioninRads), obj.D, obj.T)
-    $('.js-output-weather').html('<p> Temperature:' + obj.T + '&#176;  Wind Speed: ' + obj.S + 'm/s  Direction: ' + obj.D + '&#176;</p>')
+    $('#defaultCanvas0').show();
+};
 
-    $('#defaultCanvas0').show()
-
-}
-
-function handleWeather(city) {
-    var weather = fetchWeather(city)
-    plotWeather(weather)
-}
+let handleWeather = city => plotWeather(fetchWeather(city));
 
 // return object containing weather factors required to visualize:
 // temperature (T) in celsius and wind speed (S) in meters/second + direction (D) in degrees                //TODO CHANGE AJAX TO getJSON check async
-function fetchWeather(city) {
-    var cleanWeather = {};
+let fetchWeather = city => {
+    let cleanWeather = {};
     $.ajax({
         async: false,
         method: 'POST',
         dataType: "json",
-        url: API + '?q=' + city + queryParameters,
-        success: function (obj) {
-            console.dir(obj)
-            cleanWeather.T = (obj.main.temp) ? obj.main.temp : defaultT;
-            cleanWeather.S = (obj.wind.speed) ? obj.wind.speed : defaultS;
-            cleanWeather.D = (obj.wind.deg) ? obj.wind.deg + Dcorrection : defaultD;
+        url: CORS_WRAPPER + API + '?q=' + city + queryParameters,
+        success: res => {
+            console.dir(res)
+            cleanWeather.T = (res.main.temp) ? res.main.temp : defaultT;
+            cleanWeather.S = (res.wind.speed) ? res.wind.speed : defaultS;
+            cleanWeather.D = (res.wind.deg) ? res.wind.deg + Dcorrection : defaultD;
         },
-        error: function (e) { alert(e) }
-    })
-    // $.getJSON(API, 'q=' + city + queryParameters, function(obj) {
-    //     console.dir(obj)
-    //     cleanWeather.T = (obj.main.temp) ? obj.main.temp : defaultT;
-    //     cleanWeather.S = (obj.wind.speed) ? obj.wind.speed : defaultS;
-    //     cleanWeather.D = (obj.wind.deg) ? obj.wind.deg + Dcorrection : defaultD;
-    // })
+        error: e => console.log(e)
+    });
 
-    return cleanWeather
-}
+    return cleanWeather;
+};
 
 
 // Toggle help menu
-function transitionHelp() {
+let transitionHelp = () => {
     if ($('.js-help-text').css('display') == 'none') {
-        $('header').fadeOut('slow')
-        $('.js-help-text').fadeIn('slow')
-        $('.js-help-btn').attr('src', 'images/close.png')
+        $('header').fadeOut('slow');
+        $('.js-help-text').fadeIn('slow');
+        $('.js-help-btn').attr('src', 'images/close.png');
     } else {
-        $('.js-help-text').fadeOut("slow")
-        $('header').fadeIn("slow")
-        $('.js-help-btn').attr('src', 'images/help.png')
+        $('.js-help-text').fadeOut("slow");
+        $('header').fadeIn("slow");
+        $('.js-help-btn').attr('src', 'images/help.png');
     }
-}
+};
 
 $(function () {
-    watchForSubmit()
-    watchForRandom()
-    watchForHelp()
+    watchForSubmit();
+    watchForRandom();
+    watchForHelp();
 })
--- scripts/draw.js
+++ scripts/draw.js
@@ -1,26 +1,25 @@
 //// Globals
 
 // DOM
-var windowWidth = $('.js-background').width()
-var windowHeight = $('.js-background').height()
-var bodyMargin = 4
+let windowWidth = $('.js-background').width();
+let windowHeight = $('.js-background').height();
+let bodyMargin = 4;
 
 // Velocity
-var vFactor = 4;
+let vFactor = 4;
 
 // Particles
-var numOfParticles = 200;
-var particleRadius = 20;
-var particles = new Array(numOfParticles)
+let numOfParticles = 200;
+let particleRadius = 10;
+let particles = new Array(numOfParticles);
 
 
 //// Graphical functions
-
 function setup() {
     createCanvas(windowWidth - bodyMargin, windowHeight - bodyMargin);
-    frameRate(30)
-    for (var i = 0; i < particles.length; i++) {
-        particles[i] = new Particle()
+    frameRate(30);
+    for (let i = 0; i < particles.length; i++) {
+        particles[i] = new Particle();
     }
 }
 
@@ -28,50 +27,45 @@ function draw() {
     // only draw once a velocity from an input city has been given
     if (particles[0].v.x !== 0) {
         background(255);
-        for (var i = 0; i < particles.length; i++) {
-            particles[i].update()
-            particles[i].display()
+        for (let i = 0; i < particles.length; i++) {
+            particles[i].update();
+            particles[i].display();
         }
     }
 }
 
 function Particle() {
     // position and velocity
-    this.pos = new p5.Vector(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowWidth))
-    this.v = new p5.Vector(0, 0)
+    this.pos = new p5.Vector(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowWidth));
+    this.v = new p5.Vector(0, 0);
     this.D;
     this.T;
 
-    this.update = function () {
-        this.pos.sub(this.v)
+    this.update = () => {
+        this.pos.sub(this.v);
         if ((this.pos.x > windowWidth + 10) || (this.pos.y > windowHeight + 10) ||
             (this.pos.x < -10) || (this.pos.y < -10)) {
             this.pos.set(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowHeight));
         }
-    }
+    };
 
-    this.display = function () {
-        stroke(0)
-        fill(50)
-        ellipse(this.pos.x, this.pos.y, particleRadius, particleRadius)
-    }
+    this.display = () => {
+        let color = (this.T < 0) ? 'rgb(0,0,200)' : 'rgb(200,0,0)';
+        noStroke();
+        fill(color);
+        ellipse(this.pos.x, this.pos.y, particleRadius, particleRadius);
+    };
 }
 
 //// Helper Methods
-
-
-function setParticleParams(x, y, D, T) {
-    for (var i = 0; i < particles.length; i++) {
-        particles[i].v.set(x, y)
-        particles[i].D = D
-        particles[i].T = T
+let setParticleParams = (x, y, D, T) => {
+    for (let i = 0; i < particles.length; i++) {
+        particles[i].v.set(x, y);
+        particles[i].D = D;
+        particles[i].T = T;
     }
 }
 
-function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+let getRandom = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
 
-function toRads(deg) {
-    return deg * (Math.PI / 180)
-}
+let toRads = deg => deg * (Math.PI / 180);
