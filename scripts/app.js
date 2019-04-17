c5143014c03247fc437eb52c79f5d2a286c1e7d7
cegal <camilo.garcia.larotta@gmail.com>
2019-04-17 15:15:21 -0400

    refactor: apply editorconfig style to all files

--- scripts/app.js
+++ scripts/app.js
@@ -1,42 +1,42 @@
 //// Global variables
 
 var queryParameters = '&units=metric&APPID=6f36f42e1677237f380a89ee3b72edec'
-var API = 'http://api.openweathermap.org/data/2.5/weather'
+var API = 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather'
 var defaultT = 25;
 var defaultS = 5;
 var defaultD = 45;
 // to account for the coordinate system of p5.js
 var Dcorrection = 0;
-var defaultCities = ["Kathmandu", "Lhasa", "İstanbul", "Pya", "Hokkaidō", 
-                    "Lisbon", "Helsinki", "Osuna", "Northwich", "Villa del Rosario", 
-                    "Santa Marta", "Leticia", "Cali", "Gibraltar"]
+var defaultCities = ["Kathmandu", "Lhasa", "İstanbul", "Pya", "Hokkaidō",
+    "Lisbon", "Helsinki", "Osuna", "Northwich", "Villa del Rosario",
+    "Santa Marta", "Leticia", "Cali", "Gibraltar"]
 
 //// Event Listeners
 
 function watchForSubmit() {
-    $('.js-city-form').on('click', '.js-city-submit', function(event) {
+    $('.js-city-form').on('click', '.js-city-submit', function (event) {
         event.preventDefault()
         if (!($('.js-city-name').val())) {
-             $( ".js-city-name" ).fadeOut('fast')
-             $( ".js-city-name" ).fadeIn('fast')
+            $(".js-city-name").fadeOut('fast')
+            $(".js-city-name").fadeIn('fast')
             return;
         }
-        var inputCity = $('.js-city-name').val();   
+        var inputCity = $('.js-city-name').val();
         handleWeather(inputCity)
     })
 }
 
 function watchForRandom() {
-    $('.js-city-form').on('click', '.js-city-random', function(event){
+    $('.js-city-form').on('click', '.js-city-random', function (event) {
         event.preventDefault()
-        var randomCity = defaultCities[Math.floor(Math.random()*defaultCities.length)]
+        var randomCity = defaultCities[Math.floor(Math.random() * defaultCities.length)]
         $('.js-city-name').val(randomCity)
         handleWeather(randomCity)
     })
 }
 
 function watchForHelp() {
-    $('.js-help-btn').click(function(e) {
+    $('.js-help-btn').click(function (e) {
         transitionHelp()
     })
 }
@@ -47,7 +47,7 @@ function plotWeather(obj) {
     console.dir(obj)
     var directioninRads = toRads(obj.D);
     setParticleParams(obj.S * Math.sin(directioninRads), obj.S * Math.cos(directioninRads), obj.D, obj.T)
-    $('.js-output-weather').html('<p> Temperature:'+ obj.T + '&#176;  Wind Speed: ' + obj.S + 'm/s  Direction: ' + obj.D  +'&#176;</p>')
+    $('.js-output-weather').html('<p> Temperature:' + obj.T + '&#176;  Wind Speed: ' + obj.S + 'm/s  Direction: ' + obj.D + '&#176;</p>')
 
     $('#defaultCanvas0').show()
 
@@ -66,14 +66,14 @@ function fetchWeather(city) {
         async: false,
         method: 'POST',
         dataType: "json",
-        url: API+'?q=' + city + queryParameters,
-        success: function(obj) {
+        url: API + '?q=' + city + queryParameters,
+        success: function (obj) {
             console.dir(obj)
             cleanWeather.T = (obj.main.temp) ? obj.main.temp : defaultT;
             cleanWeather.S = (obj.wind.speed) ? obj.wind.speed : defaultS;
             cleanWeather.D = (obj.wind.deg) ? obj.wind.deg + Dcorrection : defaultD;
         },
-        error: function(e) {alert(e)}
+        error: function (e) { alert(e) }
     })
     // $.getJSON(API, 'q=' + city + queryParameters, function(obj) {
     //     console.dir(obj)
@@ -88,18 +88,18 @@ function fetchWeather(city) {
 
 // Toggle help menu
 function transitionHelp() {
-    if($('.js-help-text').css('display') == 'none'){
-            $('header').fadeOut('slow')
-            $('.js-help-text').fadeIn('slow')
-            $('.js-help-btn').attr('src', 'images/close.png')
-        } else {
-            $('.js-help-text').fadeOut("slow")
-            $('header').fadeIn("slow")
-            $('.js-help-btn').attr('src', 'images/help.png')
+    if ($('.js-help-text').css('display') == 'none') {
+        $('header').fadeOut('slow')
+        $('.js-help-text').fadeIn('slow')
+        $('.js-help-btn').attr('src', 'images/close.png')
+    } else {
+        $('.js-help-text').fadeOut("slow")
+        $('header').fadeIn("slow")
+        $('.js-help-btn').attr('src', 'images/help.png')
     }
 }
 
-$(function(){
+$(function () {
     watchForSubmit()
     watchForRandom()
     watchForHelp()
--- scripts/draw.js
+++ scripts/draw.js
@@ -17,40 +17,40 @@ var particles = new Array(numOfParticles)
 //// Graphical functions
 
 function setup() {
-    createCanvas(windowWidth-bodyMargin, windowHeight-bodyMargin);
+    createCanvas(windowWidth - bodyMargin, windowHeight - bodyMargin);
     frameRate(30)
-    for (var i=0;i<particles.length;i++) {
+    for (var i = 0; i < particles.length; i++) {
         particles[i] = new Particle()
     }
 }
- 
+
 function draw() {
     // only draw once a velocity from an input city has been given
     if (particles[0].v.x !== 0) {
         background(255);
-        for (var i=0;i<particles.length;i++) {
+        for (var i = 0; i < particles.length; i++) {
             particles[i].update()
             particles[i].display()
         }
     }
 }
 
-function Particle(){
+function Particle() {
     // position and velocity
     this.pos = new p5.Vector(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowWidth))
     this.v = new p5.Vector(0, 0)
     this.D;
     this.T;
-    
-    this.update = function() {
+
+    this.update = function () {
         this.pos.sub(this.v)
-        if ((this.pos.x > windowWidth +10) || (this.pos.y > windowHeight+10) ||
+        if ((this.pos.x > windowWidth + 10) || (this.pos.y > windowHeight + 10) ||
             (this.pos.x < -10) || (this.pos.y < -10)) {
-                this.pos.set(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowHeight));
-            }
+            this.pos.set(getRandom(-windowWidth, windowWidth), getRandom(-windowHeight, windowHeight));
+        }
     }
 
-    this.display = function() {
+    this.display = function () {
         stroke(0)
         fill(50)
         ellipse(this.pos.x, this.pos.y, particleRadius, particleRadius)
@@ -61,7 +61,7 @@ function Particle(){
 
 
 function setParticleParams(x, y, D, T) {
-    for (var i=0;i<particles.length;i++) {
+    for (var i = 0; i < particles.length; i++) {
         particles[i].v.set(x, y)
         particles[i].D = D
         particles[i].T = T
